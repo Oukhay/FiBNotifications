9d5cdeb430f137a2cd09651fb586233b7795e63c
medlink <dev@360medlink.com>
2018-02-14 20:46:41 -0500

    Reformat code

--- src/Utils/HttpClient.php
+++ src/Utils/HttpClient.php
@@ -11,23 +11,7 @@ class HttpClient
 
     public  function post($url,$data,$headers=array()){
 
-//        $url = 'https://fcm.googleapis.com/fcm/send';
-
-//        $fields = array (
-//            'registration_ids' => array (
-//                $id
-//            ),
-//            'data' => array (
-//                "message" => $message
-//            )
-//        );
         $fields = json_encode ( $data );
-
-//        $headers = array (
-//            'Authorization: key=' . "YOUR_KEY_HERE",
-//            'Content-Type: application/json'
-//        );
-
         $ch = curl_init ();
         curl_setopt ( $ch, CURLOPT_URL, $url );
         curl_setopt ( $ch, CURLOPT_POST, true );
